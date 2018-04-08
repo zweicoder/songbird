5ee6ecd3ffb43c01cbe91d94fb8c994e81c2e311
zweicoder <zweicoder@gmail.com>
2018-04-09 00:14:01 +0800

    Enforce non null constraints

--- server/migrations/InitialSchema.sql
+++ server/migrations/InitialSchema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
         id SERIAL PRIMARY KEY,
-        spotify_username TEXT UNIQUE,
-        token TEXT UNIQUE,
+        spotify_username TEXT UNIQUE NOT NULL,
+        token TEXT UNIQUE NOT NULL,
         created_at TIMESTAMPTZ DEFAULT NOW()
         );
 
@@ -9,7 +9,8 @@ CREATE TABLE users (
 CREATE TABLE subscriptions (
         id SERIAL PRIMARY KEY,
         user_id INT NOT NULL REFERENCES users(id),
-        spotify_playlist_id TEXT UNIQUE,
+        spotify_playlist_id TEXT UNIQUE NOT NULL,
         playlist_type SMALLINT NOT NULL,
-        created_at TIMESTAMPTZ DEFAULT NOW()
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        CONSTRAINT ak_user_id_playlist_type UNIQUE (user_id, playlist_type)
         );
