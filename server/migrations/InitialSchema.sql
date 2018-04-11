7eca97b507d2ad1ee40f5565c7161f3f94b18e32
zweicoder <zweicoder@gmail.com>
2018-04-12 00:26:14 +0800

    Remove unique user_id & playlist type constraint for app validation

--- server/migrations/InitialSchema.sql
+++ server/migrations/InitialSchema.sql
@@ -12,5 +12,5 @@ CREATE TABLE subscriptions (
         spotify_playlist_id TEXT UNIQUE NOT NULL,
         playlist_type SMALLINT NOT NULL,
         created_at TIMESTAMPTZ DEFAULT NOW(),
-        CONSTRAINT ak_user_id_playlist_type UNIQUE (user_id, playlist_type)
+        deleted_at TIMESTAMPTZ
         );
