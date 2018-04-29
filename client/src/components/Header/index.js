604811042b2227ba2addb998debabe9d6caa0c7d
zweicoder <zweicoder@gmail.com>
2018-04-29 19:43:47 +0800

    Hide logout button when logged out

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import logo from './songbird-clean-logo.png'
+import logo from './songbird-clean-logo.png';
 import './index.css';
-import Button from '../Button.js'
+import Button from '../Button.js';
+import { isAuthenticated } from '../../services/authService.js';
 
+const LogoutButton = () => {
+  return (
+    <div className="pull-right vertical-center">
+      <Button className="btn action-button" href="/logout">
+        Logout
+      </Button>
+    </div>
+  );
+};
 const Header = () => {
   return (
     <header className="header">
       <div className="vertical-center pull-left">
-        <img src={logo} className="logo pull-left" alt="logo"/>
+        <img src={logo} className="logo pull-left" alt="logo" />
         <h2 className="title">Songbird</h2>
       </div>
-      <div className="pull-right vertical-center">
-        <Button className="btn action-button" href="/logout">Logout</Button>
-      </div>
+      {isAuthenticated() && <LogoutButton />}
     </header>
   );
 };
