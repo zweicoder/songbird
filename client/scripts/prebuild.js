83843e2fa255a5d0b7c63ae2ae158b1a6e663f9b
zweicoder <zweicoder@gmail.com>
2018-08-19 17:43:37 +0800

    Use babel instead so symlink will not screw up spotify-service in prebuild

--- client/scripts/prebuild.js
+++ client/scripts/prebuild.js
@@ -9,7 +9,7 @@ const packagePaths = packages.map(e=> path.resolve(__dirname, e));
 console.log('Executing prebuild script...');
 packagePaths.forEach(package => {
   console.log(`Minimizing ${package}...`);
-  const res = exec(`BABEL_ENV=production babel --presets=react-app ${package} --out-dir ${package}`, {silent: true, async: true});
+  const res = exec(`babel --presets=es2015 ${package} --out-dir ${package}`, {silent: true, async: true});
   if (res.code !== 0) {
     exit(res.code);
   }
