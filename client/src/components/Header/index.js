af5d5abed971f77fc2e8a31ef2ff27f13ff85493
zweicoder <zweicoder@gmail.com>
2018-09-09 22:48:25 +0800

    Add simple $2 Donate button

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -31,15 +31,47 @@ const PremiumButton = ({ location }) => {
   );
 };
 
-const Header = ({ location, history }) => {
+const DonateButton = () => {
+  return (
+    <div className="pull-right vertical-center">
+      <form
+        action="https://www.paypal.com/cgi-bin/webscr"
+        method="post"
+        target="_top"
+      >
+        <input type="hidden" name="cmd" value="_s-xclick" />
+        <input type="hidden" name="hosted_button_id" value="8YDJC378TR7HL" />
+        <input
+          type="image"
+          src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif"
+          border="0"
+          name="submit"
+          alt="PayPal – The safer, easier way to pay online!"
+        />
+        <img
+          alt=""
+          border="0"
+          src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif"
+          width="1"
+          height="1"
+        />
+      </form>
+    </div>
+  );
+};
 
+const Header = ({ location, history }) => {
   return (
     <header className="header">
-      <div className="vertical-center pull-left logo-container" onClick={() => history.push('/')}>
+      <div
+        className="vertical-center pull-left logo-container"
+        onClick={() => history.push('/')}
+      >
         <img src={logo} className="logo pull-left" alt="logo" />
         <h2 className="title">Songbird</h2>
       </div>
       <LogoutButton />
+      <DonateButton />
     </header>
   );
 };
